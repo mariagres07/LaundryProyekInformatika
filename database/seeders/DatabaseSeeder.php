dea150e1d0e1ec3ee5a88f6a1ff60a518d57bb56
mariagres07 <mariagresia347@gmail.com>
2025-09-22 16:21:12 +0700

    database nya diperbaiki

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,10 +25,10 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
 
         $kurir = Kurir::factory()->create([
             'namaKurir' => 'Kurir 1',
