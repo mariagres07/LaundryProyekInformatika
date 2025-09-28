6fdb5c90299eed60822b8bd817f8d8da88eb1455
mariagres07 <mariagresia347@gmail.com>
2025-09-28 11:35:04 +0700

    bikin pesan laundry view, controller, dan ada modifikasi di route nya

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PesanLaundryController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// untuk pesanLaundry.blade.php
+Route::get('/pesanLaundry', [PesanLaundryController::class, 'index'])->name('pesanLaundry');
+
+// untuk detailPesanan.blade.php
+Route::get('/detailPesanan', [PesanLaundryController::class, 'detail'])->name('detailPesanan');
+
+// proses checkout (alamat + paket)
+Route::post('/checkout', [PesanLaundryController::class, 'checkout'])->name('checkout');
