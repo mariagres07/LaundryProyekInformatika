b3d87cae19f4bd3c5bb8034736c9e706a6195185
mariagres07 <mariagresia347@gmail.com>
2025-11-12 09:36:52 +0700

    tambah update status di ClihatPesanan, proses pembayaran di PembayaranController, add ubahStatus di model pesanan, add route pembayaran sukses

--- routes/web.php
+++ routes/web.php
@@ -65,8 +65,9 @@ Route::post('/kurir/konfirmasi-berat/{idPesanan}', [KurirController::class, 'kon
     ->name('kurir.konfirmasiBerat');
 
 // ===================== PEMBAYARAN =====================
-Route::post('/pembayaran/{idPesanan}', [PembayaranController::class, 'prosesPembayaran'])->name('pembayaran.proses');
 Route::get('/pembayaran/{idPesanan}', [PembayaranController::class, 'index'])->name('pembayaran.index');
+Route::post('/pembayaran/{idPesanan}', [PembayaranController::class, 'prosesPembayaran'])->name('pembayaran.proses');
+Route::get('/pembayaran/success', [PembayaranController::class, 'success'])->name('pembayaran.success');
 
 // ===================== SELESAIKAN PESANAN =====================
 Route::post('/pesanan/{id}/selesai', [CVerifikasi::class, 'selesaikan'])->name('pesanan.selesai');
